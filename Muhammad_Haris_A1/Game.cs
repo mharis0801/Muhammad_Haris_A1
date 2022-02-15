c3f90dedfa1b509d155dac54797101fb4399e80b
Haris <harismu@sheridancollege.ca>
2022-02-15 17:40:53 +0300

    Basic user interactions implemented and the counters work

--- Muhammad_Haris_A1/Game.cs
+++ Muhammad_Haris_A1/Game.cs
@@ -7,32 +7,39 @@ namespace Muhammad_Haris_A1
         public int Die2 { get; private set; }
         public int TurnScore { get; private set; }
         public int OverallScore { get; private set; }
-        public int PlayCount { get; private set; } = 0;
+        public int PlayCount { get; private set; } 
 
 
         public Game()
         {
+            Roll();
+
+            PlayCount = 0;
         }
         
         public void Roll()
         {
             Random DieNumGen = new Random();
-            Die1 = DieNumGen.Next(0, 7);
-            Die2 = DieNumGen.Next(0, 7);
+            Die1 = DieNumGen.Next(1, 7);
+            Die2 = DieNumGen.Next(1, 7);
+            PlayCount++;
 
             if (Die1 == 6 & Die2 == 6)
             {
                 TurnScore = 100;
+                
             }
             else if (Die1 == Die2)
             {
                 TurnScore = Die1 * 10;
+                
             }
             else
                 TurnScore = 0;
 
-            OverallScore += TurnScore;
-            PlayCount++;
+            OverallScore = TurnScore + OverallScore;
+            
+            
         }
     }
 }
--- Muhammad_Haris_A1/MainPage.xaml.cs
+++ Muhammad_Haris_A1/MainPage.xaml.cs
@@ -9,10 +9,32 @@ using Xamarin.Forms;
 namespace Muhammad_Haris_A1
 {
     public partial class MainPage : ContentPage
+        
     {
+        private Game _game;
+
         public MainPage()
         {
             InitializeComponent();
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            _game = new Game();
+            
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+            _game.Roll();
+            startMsg.Text = $"times played {_game.PlayCount}";
+            Die1Veiw.Text = $"{_game.Die1}";
+            Die2Veiw.Text = $"{_game.Die2}";
+            turnScore.Text = $"Turn Score = {_game.TurnScore}";
+            overallScore.Text = $"Overall Score = {_game.OverallScore}";
+            
         }
     }
 }
